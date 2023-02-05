48ae7a7d256881d31674daeb5a7976ecb0b843db
johnszyna <jan.szyndlarewicz@gmail.com>
2023-02-05 21:34:04 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.vocabularyapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.vocabularyapp to javafx.fxml;
+    exports com.example.vocabularyapp;
+}
